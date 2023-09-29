55257f786c8811283f1d9e257f134ec0ab77c371
Jehan Patel <90050088+JehanPatel@users.noreply.github.com>
2023-09-29 22:33:57 +0530

    Add files via upload

---
+++ HackerRank/mini max sum.py
@@ -0,0 +1,23 @@
+import math
+import os
+import random
+import re
+import sys
+
+
+def miniMaxSum(arr):
+    arr.sort()
+    hold = [None]*int(len(arr)-3)
+    for i in range(0,len(arr)-3):
+        temp = 0
+        for j in range(i,i+4):
+            temp = temp + arr[j]
+        hold[i] = temp
+    
+    print(hold[0],hold[-1])
+
+
+if __name__ == '__main__':
+    arr = list(map(int, input().rstrip().split()))
+
+    miniMaxSum(arr)
