aab0a079ae8cba194aa02675609d65a87cbfcc8c
JehanPatel <jehan.patel2021@vitbhopal.ac.in>
2023-10-14 23:14:45 +0530

    added CodeChef, HackerEarth, HackerRank, LeetCode solutions

---
+++ HackerEarth/Determining Numbers.py
@@ -0,0 +1,13 @@
+from collections import Counter
+
+n = int(input())
+arr = list(map(int,input().split()))
+a = Counter(arr)
+ans = []
+for k,v in a.items():
+    if v==1:
+        ans.append(k)
+        
+ans.sort()
+        
+print(*ans)
