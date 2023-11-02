719343ec3dea6f3c03439bb4a585b3b7db0272ca
JehanPatel <jehan.patel2021@vitbhopal.ac.in>
2023-11-02 23:35:50 +0530

    added CodeChef, HackerEarth, HackerRank, LeetCode solutions

---
+++ CodeChef/How to accept string inputs.py
@@ -0,0 +1,6 @@
+# Solution as follows
+
+A, B = input().split()
+C, D, E = input().split()
+F, G, H, I = input().split()
+print(A, B, C, D, E, F, G, H, I)
---
+++ CodeChef/How to print outputs.py
@@ -0,0 +1,10 @@
+# Solution as follows
+
+N = 5
+print(N)
+
+S = "abcde"
+print(S)
+
+Arr = ["abc", 1, 34, "name"]
+print(Arr)
