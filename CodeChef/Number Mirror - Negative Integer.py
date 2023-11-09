41968f597026e6847620103f639a7846dc7170e6
JehanPatel <jehan.patel2021@vitbhopal.ac.in>
2023-11-09 23:53:47 +0530

    added CodeChef, HackerEarth, HackerRank, LeetCode solutions

---
+++ CodeChef/Number Mirror - Negative Integer.py
@@ -0,0 +1,10 @@
+# Solution as follows
+
+#accept the count of test cases given in the the 1st line
+t = int(input())        
+#run a loop to accept 't' inputs
+for i in range(t):      
+    #accept 1 integer on the 1st line of each test case
+    N = int(input())    
+    #output the negative integer - i.e. (-N)
+    print(0-N)          
