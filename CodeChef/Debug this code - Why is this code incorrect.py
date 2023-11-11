a863358a8809e2d956eb99d8baf1611c01ed3046
JehanPatel <jehan.patel2021@vitbhopal.ac.in>
2023-11-12 00:12:05 +0530

    added CodeChef, HackerEarth, HackerRank, LeetCode solutions

---
+++ CodeChef/Debug this code - Why is this code incorrect.py
@@ -0,0 +1,8 @@
+# Solution as follows
+
+#without 'int', Python assumes input to be a string
+t = int(input())        
+#without ':', you will get a compilation error
+for i in range(t):      
+    N = int(input())
+    print(2*N)
