6e8c2fac7cb1888c0a45ebb5dce0315f1d4381b6
JehanPatel <jehan.patel2021@vitbhopal.ac.in>
2023-11-12 18:46:21 +0530

    added CodeChef, HackerEarth, HackerRank, LeetCode solutions

---
+++ CodeChef/Debug this code - Why is this code incorrect 2.py
@@ -0,0 +1,7 @@
+# Solution as follows
+
+t = int(input())
+for i in range(t):
+    N = int(input())
+    #without this indentation, 'print' command was being executed outside the for loop
+    print(2*N)          
