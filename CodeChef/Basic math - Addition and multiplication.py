664a438f1e7fb010b715b9a462d91b0c3d9afab1
JehanPatel <jehan.patel2021@vitbhopal.ac.in>
2023-11-13 23:53:50 +0530

    added CodeChef, HackerEarth, HackerRank, LeetCode solutions

---
+++ CodeChef/Basic math - Addition and multiplication.py
@@ -0,0 +1,12 @@
+# Solution as follows
+
+t = int(input())
+for i in range(t):
+    #Accept 2 integers inputs
+    A, B = map(int, input().split())     
+    #Sum of inputs
+    S = A + B               
+    #Product of inputs
+    P = A * B               
+    #Print the desired output for each test case
+    print(S,P)              
