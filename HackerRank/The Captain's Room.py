f955e631f9ff42e350333b797bf6dd6a3162ffde
JehanPatel <jehan.patel2021@vitbhopal.ac.in>
2023-11-30 17:52:50 +0530

    added CodeChef, HackerEarth, HackerRank, LeetCode solutions

---
+++ HackerRank/The Captain's Room.py
@@ -0,0 +1,8 @@
+if __name__ == '__main__':
+    k = int(input())
+    rooms = list(map(int, input().split()))
+    roomSet = set(rooms)
+    roomSum = sum(rooms)
+    roomSetSum = sum(roomSet) * k
+    captiansRoom = (roomSetSum - roomSum) // (k - 1)
+    print(captiansRoom)
