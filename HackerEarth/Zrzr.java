62c0507205d314afee8a9cb87a94095606fbe2af
JehanPatel <jehan.patel2021@vitbhopal.ac.in>
2023-12-06 23:19:54 +0530

    added CodeChef, HackerEarth, HackerRank, LeetCode solutions

---
+++ HackerEarth/Zrzr.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class ZrZr {
+	public static void main(String[] arg){
+		Scanner sc=new Scanner(System.in);
+		StringBuffer sb=new StringBuffer();
+		int t=sc.nextInt();
+		while (t-->0){
+			long n=sc.nextLong();
+			sb.append(((n/5)+(n/25))+"\n");
+		}
+		System.out.println(sb);
+		
+	}
+}
