1e44e0e725e41de219e11bc8e984996fcc50d01e
JehanPatel <jehan.patel2021@vitbhopal.ac.in>
2023-12-20 00:39:59 +0530

    added CodeChef, HackerEarth, HackerRank, LeetCode solutions

---
+++ CodeChef/Try to solve this problem using checkpoints.py
@@ -0,0 +1,11 @@
+for i in range(5, 0, -1):
+    for j in range(1, i + 1):
+        print("*", end="")
+    print()
+
+# Checkpoint1 - first half of the code output
+
+for i in range(2, 6):  # Changed i to 2
+    for j in range(1, i + 1):
+        print("*", end="")
+    print()
---
+++ HackerEarth/Anti Palindrome Strings.py
@@ -0,0 +1,9 @@
+
+t = int(input())
+for _ in range(t):
+    inp = input()
+    s = ''.join(sorted(inp))
+    if s == s[::-1]:
+        print(-1)
+    else:
+        print(s)
